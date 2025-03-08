f5919ce22ae80500db4144697a58b2d6cda0ecf1
dorazi0423 <dorazi0423@gmail.com>
2025-03-08 21:46:40 +0900

    feat: SalesData.java에 통계 출력 메서드 추가 - 통계 계산시 상품 개수 저장하는 메서드 추가 - 통계부분에서 상품별로 출력하는 메서드 추가

--- src/main/java/controller/SalesData.java
+++ src/main/java/controller/SalesData.java
@@ -47,9 +47,11 @@ public class SalesData {
 	// 특정 날짜에 대한 제품 판매량 초기화(처음에 날짜 데이터값 없을 때)
 	private void initializeDateItemMap(LocalDate date) {
 		Map<Product, Integer> productSales = new HashMap<>();
+		
 		for (Product product : productList) {
 			productSales.put(product, 0);
 		}
+		
 		transactionMap.put(date, productSales);
 	}
 
@@ -67,10 +69,49 @@ public class SalesData {
 		}
 	}
 
-	//년도별 통계
-	public void getAnnualReport() {
-		System.out.println();
+	
+	//통계 계산할 때, 중간에 상품 개수 저장하는 맵 업데이트하는 기능
+	public void updateproductTotal(Map<Product, Integer> productMap, Map<Product, Integer> productTotal) {//상품구매정보 맵, 상품에 통계 저장할 맵
+		for (Map.Entry<Product, Integer> productEntry : productMap.entrySet()) {
+			int count = productEntry.getValue(); //음식 판매 개수
+			Product key = productEntry.getKey();//키 값, 상품객체
+	
+			productTotal.put(key, productTotal.getOrDefault(key, 0) + count);//키값이 없으면 0, 아니면 count값을 더함
+		}
+	}
+	
+	//통계부분에서 상품별로 개수 출력
+	public void printProductReport(Map<Product, Integer> productTotal) {
+		System.out.println("[상품명]\t\t[가격]\t\t[수량]\t\t[매출]\t\t");
 		
+		int totalCount = 0; //
+		int totalSales =0;
+		
+		for(Map.Entry<Product, Integer> productEntry : productTotal.entrySet()) {
+			StringBuffer bf = new StringBuffer();
+			String name = productEntry.getKey().getName(); //음식이름
+			int price = productEntry.getKey().getPrice(); //음식가격
+			int count = productEntry.getValue(); //음식 판매 개수
+			int sales = price*count; // 매출
+			
+			totalCount += count; //판매수량 계산
+			totalSales += price*count; //매출 계산
+			
+			bf.append(name + "\t\t");
+			bf.append(price + "원\t\t");
+			bf.append(count + "개\t\t");
+			bf.append(sales + "원\t\t");
+			
+			System.out.println(bf);
+		}
+		System.out.println("------------------------------------------------------");
+		System.out.println("[총 판매 수량]: " + totalCount + "개");
+		System.out.println("[총 매출]: " + totalSales + "원");
+	}
+	
+	//년도별 통계
+	public void getYearReport() {
+
 	}
 	
 	//월별 통계
@@ -78,40 +119,18 @@ public class SalesData {
 		
 	}
 	
+	
 	//일별 통계
 	public void getDailyReport() {
 		System.out.println("===================날짜별 매출 통계======================");
 		for (Map.Entry<LocalDate, Map<Product, Integer>> entry : transactionMap.entrySet()) {
 			LocalDate date = entry.getKey();
-			Map<Product, Integer> productMap = entry.getValue();
-			int totalDailyCount = 0;
-			int totalDailySales =0;
+			Map<Product, Integer> productTotal = entry.getValue();
 		
 			System.out.println("======================================================");
 			System.out.println("[날짜: " + date + "]");
-			System.out.println("[상품명]\t\t[가격]\t\t[수량]\t\t[매출]\t\t");
 			
-			for (Map.Entry<Product, Integer> productEntry : productMap.entrySet()) {
-				StringBuffer bf = new StringBuffer();
-				String name = productEntry.getKey().getName(); //음식이름
-				int price = productEntry.getKey().getPrice(); //음식가격
-				int count = productEntry.getValue(); //음식 판매 개수
-				int sales = price*count; // 매출
-				
-				totalDailyCount += count; //일별 판매수량 계산
-				totalDailySales += price*count; //일별 매출 계산
-				
-				bf.append(name + "\t\t");
-				bf.append(price + "원\t\t");
-				bf.append(count + "개\t\t");
-				bf.append(sales + "원\t\t");
-				
-				System.out.println(bf);
-			}
-			
-			System.out.println("------------------------------------------------------");
-			System.out.println("[총 판매 수량]: " + totalDailyCount + "개");
-			System.out.println("[총 매출]: " + totalDailySales + "원");
+			printProductReport(productTotal);
 		}
 		System.out.println("======================================================");
 	}
