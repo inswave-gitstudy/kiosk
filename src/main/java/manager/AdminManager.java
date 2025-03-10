0a37862bd6c3b4fcf593b91534c8a4ec1810638f
dorazi0423 <dorazi0423@gmail.com>
2025-03-10 16:27:21 +0900

    feat: 비밀번호 salt 기능 추가 - AdminManager.java에 Salt 함수 추가 - AdminManager.java 암호화 기능에 salt 추가 - AdminManager.java에 salt를 불러오는 기능 추가 - AdminManager.java 파일 저장기능 수정

--- src/main/java/manager/AdminManager.java
+++ src/main/java/manager/AdminManager.java
@@ -1,21 +1,27 @@
 package manager;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Scanner;
 
 import model.Admin;
 
 public class AdminManager {
-	Scanner sc;
+	private Scanner sc;
 	private Admin admin;
+	private String salt;
 	
 	public AdminManager() {
 		this.admin = new Admin();
 		this.sc = new Scanner(System.in);
+		this.salt = saltLoad();
 		
 		if(admin.getPassword() == null) {
 			initializePassword("admin74");
@@ -24,7 +30,7 @@ public class AdminManager {
 	
 	//로그인 기능
     public boolean checkLoginCredentials(String inputPassword) {
-        String hashPassword = encryptPassword(inputPassword);
+        String hashPassword = encryptPassword(inputPassword, this.salt);
 
         if(hashPassword.equals(admin.getPassword()))
         	return true;
@@ -43,8 +49,9 @@ public class AdminManager {
             	password = sc.nextLine();
             	
     			if(checkPassword(password.trim())) {
-    				String newHashPassword = encryptPassword(password.trim());
-    	        	savePasswordFile(newHashPassword); //비밀번호를 암호화후에 파일에 저장
+    				String salt = generateSalt();
+    				String newHashPassword = encryptPassword(password.trim(), salt);
+    	        	savePasswordFile(newHashPassword, salt); //비밀번호를 암호화후에 파일에 저장
     	        	admin.setPassword(newHashPassword);//Admin 클래스에 있는 비밀번호를 새로운 비밀번호로 수정
     	        	
     	        	System.out.println("비밀번호 수정되었습니다.");
@@ -111,13 +118,14 @@ public class AdminManager {
     
     //초기에 비밀번호 초기화 하는 기능
     public void initializePassword(String password) {
-    	String pin = encryptPassword(password);
-    	savePasswordFile(pin);
+    	String salt = generateSalt();
+    	String pin = encryptPassword(password, salt);
+    	savePasswordFile(pin, salt);
     	admin.setPassword(pin);
     }
     
-    // 비밀번호를 SHA-256으로 암호화하는 메서드
-    private String encryptPassword(String password)  { 
+    // 비밀번호를 SHA-256으로 암호화하는 메서드, 단방향 암호화 방법이라 복호화가 불가능
+    private String encryptPassword(String password, String salt)  { 
         MessageDigest digest = null;
         
 		try {
@@ -127,27 +135,37 @@ public class AdminManager {
 			System.out.println("암호화 과정중에 문제가 발생했습니다" + e.getMessage());
 		}
 		
-        byte[] hashBytes = digest.digest(password.getBytes());
+        byte[] hashBytes = digest.digest((password+salt).getBytes());
         
         // Base64로 인코딩하여 문자열로 반환
         return Base64.getEncoder().encodeToString(hashBytes);
     }
     
     // 암호화된 비밀번호를 파일에 저장
-    private void savePasswordFile(String password) {
+    private void savePasswordFile(String password, String salt) {
     	String FileName = "AdminPassword.txt";
     	FileWriter fw = null;
     	BufferedWriter bw = null;
+    	
+    	String FileName2 = "Salt.txt";
+    	FileWriter fw2 = null;
+    	BufferedWriter bw2 = null;
     	
         try {
         	fw = new FileWriter(FileName);
         	bw = new BufferedWriter(fw);
             bw.write(password);
+    
+            fw2 = new FileWriter(FileName2);
+        	bw2 = new BufferedWriter(fw2);
+            bw2.write(salt);
         } catch (IOException e) {
             System.out.println("파일 저장 중 오류가 발생했습니다.");
             e.printStackTrace();
         }finally {
         	try {
+        		bw2.close();
+        		fw2.close();
 				bw.close();
 				fw.close();
 			} catch (IOException e) {
@@ -155,4 +173,52 @@ public class AdminManager {
 			}
         }
     }
+    
+    //Salt 생성
+    private String generateSalt() {
+		//랜덤 salt 생성
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[20];
+		
+		//난수를 생성
+		random.nextBytes(salt);
+		
+		//문자열로 변환
+		StringBuilder sb = new StringBuilder();
+		for(byte b : salt) {
+			sb.append(String.format("%02x", b));
+		}
+		
+		return sb.toString();
+	}
+
+    //salt를 로드해서 저장하는 기능
+    private String saltLoad() {
+    	File file = new File("Salt.txt");
+    	FileReader fr = null;
+		BufferedReader br = null;
+		String salt = "";
+		
+		if(!file.exists()) {
+			return null;
+		}
+		
+		try {
+			fr = new FileReader(file);//비밀번호 저장한 파일
+			br = new BufferedReader(fr);
+			
+			salt = br.readLine();
+		} catch (Exception e) {
+			System.out.println("비밀번호 기능에 문제가 발생했습니다." + e.getMessage());
+		}finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (Exception e2) {
+				
+			}
+		}
+    	return salt; //파일에 저장되어있는 암호화된 비밀번호를 리턴
+    }
+ 
 }
