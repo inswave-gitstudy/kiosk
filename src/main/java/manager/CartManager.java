dc7eadc1a4e974c9e2269717f82399d695c56aac
seungho <qswa97@gmail.com>
2025-03-11 15:02:48 +0900

    Cart 클래스 삭제후 , 모델 - 매니져 - 컨트롤러 방식으로 변경 추가로 , Dessert 클래스에서 옵션에 따른 분류 위해서 equals(), hashCode() 추가했음.

---
+++ src/main/java/manager/CartManager.java
@@ -0,0 +1,45 @@
+package manager;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+import model.Cart;
+import model.Product;
+
+@Getter
+public class CartManager {
+	
+	Cart cart;
+	
+	public CartManager(Cart cart) {
+		this.cart = cart;
+	}
+		
+	public void addProduct(Product product){
+		cart.addProduct(product);
+	}
+	
+	public void deleteProduct(Product product) {
+		cart.removeProduct(product);
+	}
+	
+	public List<Product> getMatchingProducts(long productId) {
+		return cart.getCartItems().keySet().stream()
+                .filter(p -> p.getProductId() == productId)
+                .collect(Collectors.toList());
+        
+    }
+	
+	public void updateQuantity(Product product, int quantity) {
+		cart.updateQuantity(product, quantity);
+	}
+	
+	public int calTotalPrice() {
+		return cart.calTotalPrice();
+	}
+	
+	
+	
+	
+}
