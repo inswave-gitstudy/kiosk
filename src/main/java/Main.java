96079542fbee1d8e906108ba25f08ead3ac685b2
keymanzer <xmwks0527@naver.com>
2025-03-11 15:51:34 +0900

    ProductManager ID입력 빼기, 출력 문구 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import controller.MainController;
+import manager.ProductManager;
 
 public class Main {
     public static void main(String[] args) {
-        MainController mainController = new MainController();
-        mainController.start();
+        //MainController mainController = new MainController();
+        //mainController.start();
+    	ProductManager pm1 = new ProductManager();
+    	pm1.run();
     }
 }
