2ad602187dd87af2cdadab603bb6daa4b59ae7ac
seungho <qswa97@gmail.com>
2025-03-12 16:12:30 +0900

    전체 판매 순위 + 베스트 셀러 + 워스트 셀러를 보여주는 기능 추가 BestSellerProduct, BestSellerProductController, BestSellerProudctManager 추가했습니다..

---
+++ src/main/java/controller/BestSellerProductController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import manager.BestSellerProductManager;
+import java.util.*;
+import model.Product;
+
+public class BestSellerProductController {
+	
+	BestSellerProductManager bestSellerProductManager;
+	
+	public BestSellerProductController(OrderController orderController) {
+		this.bestSellerProductManager = new BestSellerProductManager(orderController);
+		this.bestSellerProductManager.rank();
+	}
+	
+	// 전체 판매 순위(랭크) 가져오기
+	public List<Map.Entry<Product, Integer>> getRank(){
+		return bestSellerProductManager.getRank();
+	}
+	
+	// 가장 많이 팔린 상품 가져오기
+	public Map.Entry<Product, Integer> getBestProduct() {
+		return this.bestSellerProductManager.getBestProduct();
+	}
+	
+	// 가장 적게 팔린 상품 가져오기
+	public Map.Entry<Product, Integer> getWorstProduct() {
+		return this.bestSellerProductManager.getWorstProduct();
+	}
+	
+	// 전체 판매 순위(랭크) 보여주기
+	public void showRank() {
+		for(Map.Entry<Product, Integer> entry : this.bestSellerProductManager.getRank()) {
+			System.out.println("========= 전체 판매 순위 =========");
+			System.out.println("상품 번호 : " + entry.getKey().getProductId() +
+							    " 상품명 : " + entry.getKey().getName() +
+							    " 총 판매 수량 : "+ entry.getValue());
+		}
+	}
+	
+	// 가장 많이 팔린 상품 보여주기
+	public void showBestProduct() {
+		System.out.println("가장 많이 팔린 상품 : ");
+		System.out.println("상품 번호 : " + this.bestSellerProductManager.getBestProduct().getKey().getProductId() +
+			    " 상품명 : " + this.bestSellerProductManager.getBestProduct().getKey().getName() +
+			    " 총 판매 수량 : "+ this.bestSellerProductManager.getBestProduct().getValue());
+		
+	}
+	
+	// 가장 적게 팔린 상품 보여주기
+	public void showWorstProduct() {
+		System.out.println("가장 많이 팔린 상품 : ");
+		System.out.println("상품 번호 : " + this.bestSellerProductManager.getWorstProduct().getKey().getProductId() +
+			    " 상품명 : " + this.bestSellerProductManager.getWorstProduct().getKey().getName() +
+			    " 총 판매 수량 : "+ this.bestSellerProductManager.getWorstProduct().getValue());
+	}
+	
+
+}
---
+++ src/main/java/manager/BestSellerProductManager.java
@@ -0,0 +1,68 @@
+package manager;
+
+import java.util.*;
+
+import controller.OrderController;
+import model.BestSellerProduct;
+import model.Order;
+import model.Product;
+import repository.OrderRepository;
+import repository.TxtOrderRepository;
+
+// loadOrder()
+
+
+public class BestSellerProductManager{
+	
+	BestSellerProduct bestSellerProduct;
+	OrderController orderController;
+	
+	public BestSellerProductManager(OrderController orderController) {
+		this.bestSellerProduct = new BestSellerProduct();
+		this.orderController = orderController;
+	}
+	
+	// txt 파일을 불러와서 정보를 읽고 , 판매량순으로 정렬하고 bestSellerProduct 의 products에 set..
+	public void rank(){
+		
+		// 주문 완료 처된 order.txt 파일에서 주문 목록을 불러옴.
+		Map<Integer, Order> orders = new TreeMap<>();
+		orders = orderController.loadOrderWithTxt();
+		
+		// 각 상품의 판매량을 담을 Map
+		Map<Product, Integer> eachProductSales = new HashMap<>();
+
+		
+		for(Order order : orders.values()) {
+			
+			for(Map.Entry<Product, Integer> entry : order.getProducts().entrySet()) {
+				Product product = entry.getKey();
+				int quantity = entry.getValue();
+				
+				eachProductSales.put(product, eachProductSales.getOrDefault(product, 0) + quantity);
+			}
+		}
+		
+		// 많이 팔린 상품순으로 나열..
+		List<Map.Entry<Product, Integer>> list = new ArrayList<>(eachProductSales.entrySet());
+		list.sort((o1, o2) -> o2.getValue() - o1.getValue());
+		
+		this.bestSellerProduct.setProducts(list);
+	}
+	
+	public Map.Entry<Product, Integer> getBestProduct() {
+		return this.bestSellerProduct.getBestProduct();
+	}
+	
+	public Map.Entry<Product, Integer> getWorstProduct() {
+		return this.bestSellerProduct.getWorstProduct();
+	}
+	
+	public List<Map.Entry<Product, Integer>> getRank(){
+		return this.bestSellerProduct.getProducts();
+	}
+	
+	
+}
+
+
